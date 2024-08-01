9a10fbbb99e585a5dbf2e77b196502e44b9f10bf
shineTripodcat <111100023+shineTripodcat@users.noreply.github.com>
2024-08-02 00:52:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,16 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 删除原版 luci-app-passwall 软件包
+rm -rf feeds/luci/applications/luci-app-passwall
+
+# 克隆指定软件包的远程仓库
+git clone https://github.com/xiaorouji/openwrt-passwall feeds/passwall
+
+# 更新和安装指定的软件包
+cd openwrt
+./scripts/feeds update passwall
+./scripts/feeds install -a -p passwall
+
+# 将 luci-app-passwall 从 feeds/passwall/luci-app-passwall 复制到 feeds/luci/applications/luci-app-passwall
+cp -r feeds/passwall/luci-app-passwall feeds/luci/applications/luci-app-passwall
