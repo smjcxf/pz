7365b9c339eed2d0f6941377d9f1537536c5542c
shineTripodcat <111100023+shineTripodcat@users.noreply.github.com>
2024-08-02 21:04:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,15 +12,3 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-# 删除原版 luci-app-passwall 软件包
-rm -rf feeds/luci/applications/luci-app-passwall
-
-# 将 luci-app-passwall 从 feeds/passwall 复制到 feeds/luci/applications
-cp -r feeds/passwall/luci-app-passwall feeds/luci/applications/luci-app-passwall
-
-# 更新并安装 passwall feed 中的所有包
-./scripts/feeds update passwall
-./scripts/feeds install -a -p passwall
-
-# 安装 feeds/luci/applications 中的 luci-app-passwall
-./scripts/feeds install luci-app-passwall
