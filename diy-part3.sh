2666c03498084548c963f7b5145a7e1e4ed4e657
shineTripodcat <111100023+shineTripodcat@users.noreply.github.com>
2024-08-02 21:10:13 +0800

    Create diy-part3.sh

---
+++ diy-part3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# File name: diy-part3.sh
+# Description: OpenWrt DIY script part 3 (After Update feeds)
+#
+
+# 删除现有的 luci-app-passwall 软件包
+rm -rf feeds/luci/applications/luci-app-passwall
+
+# 克隆新的 luci-app-passwall 软件包
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+
+# 移动克隆的文件到正确的目录
+mv package/luci-app-passwall/luci-app-passwall feeds/luci/applications/
+
+# 删除临时克隆目录
+rm -rf package/luci-app-passwall
