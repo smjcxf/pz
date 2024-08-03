c6483dde8ee6ad9fae1f327aa9c59722b041aefb
shineTripodcat <111100023+shineTripodcat@users.noreply.github.com>
2024-08-03 23:13:48 +0800

    Update diy-part3-imm.sh

--- diy-part3-imm.sh
+++ diy-part3-imm.sh
@@ -18,6 +18,8 @@
 # 删除 /feeds/passwall_package/sing-box 目录
 #rm -rf feeds/passwall_package/sing-box
 
+./scripts/feeds update -a
+
 #rm -rf feeds/luci/applications/luci-app-mosdns
 #rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 #rm -rf feeds/packages/utils/v2dat
