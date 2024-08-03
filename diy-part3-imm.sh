d1a995349b8cd6c192ac0a6fcf2f882e9804b168
shineTripodcat <111100023+shineTripodcat@users.noreply.github.com>
2024-08-03 19:51:54 +0800

    Update diy-part3-imm.sh

--- diy-part3-imm.sh
+++ diy-part3-imm.sh
@@ -8,18 +8,18 @@
 rm -rf feeds/luci/applications/luci-app-passwall
 
 # 克隆新的 luci-app-passwall 软件包
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 
 # 移动克隆的文件到正确的目录
-#mv package/luci-app-passwall/luci-app-passwall feeds/luci/applications/
+mv package/luci-app-passwall/luci-app-passwall feeds/luci/applications/
 
 # 删除临时克隆目录
-#rm -rf package/luci-app-passwall
+rm -rf package/luci-app-passwall
 # 删除 /feeds/passwall_package/sing-box 目录
 #rm -rf feeds/passwall_package/sing-box
 
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+#rm -rf feeds/luci/applications/luci-app-mosdns
+#rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+#rm -rf feeds/packages/utils/v2dat
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
