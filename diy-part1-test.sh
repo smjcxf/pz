fdf89139dd6168b45c5381c12a87a01149519472
shineTripodcat <111100023+shineTripodcat@users.noreply.github.com>
2024-08-03 14:06:50 +0800

    更新 diy-part1-test.sh

--- diy-part1-test.sh
+++ diy-part1-test.sh
@@ -13,6 +13,7 @@
 # Add a feed source
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
 echo 'src-git passwall_package https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+echo 'src-git luci_lua_runtime https://github.com/openwrt/luci/tree/master/modules/luci-lua-runtime' >>feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
