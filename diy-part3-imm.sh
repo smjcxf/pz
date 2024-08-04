c328f5e6ed8579756c39cb6409fec6d08497fc82
shineTripodcat <111100023+shineTripodcat@users.noreply.github.com>
2024-08-04 08:51:37 +0800

    Update diy-part3-imm.sh

--- diy-part3-imm.sh
+++ diy-part3-imm.sh
@@ -20,10 +20,4 @@ rm -rf feeds/luci/applications/luci-app-passwall
 # 删除 /feeds/passwall_package/sing-box 目录
 #rm -rf feeds/passwall_package/sing-box
 
-#rm -rf feeds/luci/applications/luci-app-mosdns
-#rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-#rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-
 ./scripts/feeds update -a
