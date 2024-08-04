2467daad5e4fba7819d4116928e3ac20fb0e883b
shineTripodcat <111100023+shineTripodcat@users.noreply.github.com>
2024-08-04 08:19:10 +0800

    Update diy-part1-imm.sh

--- diy-part1-imm.sh
+++ diy-part1-imm.sh
@@ -9,19 +9,22 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+
 git clone https://github.com/openwrt/openwrt
+git clone https://github.com/immortalwrt/luci
+
 mv openwrt/package/utils/ucode package/utils/
-rm -rf openwrt
+mv luci/contrib/package/ucode-mod-lua feeds/luci/contrib/package/
+
+rm -rf openwrt luci
 
 # Add a feed source
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
 echo 'src-git passwall_package https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 #echo 'src-git luci_lua_runtime https://github.com/openwrt/luci/tree/master/modules/luci-lua-runtime' >>feeds.conf.default
-#echo 'src-git luci_lua_runtime https://github.com/shineTripodcat/runtime' >>feeds.conf.default
-#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '2d' feeds.conf.default
-sed -i '2i src-git luci https://github.com/shineTripodcat/luci.git;openwrt-21.02' feeds.conf.default
+echo 'src-git luci_lua_runtime https://github.com/shineTripodcat/runtime' >>feeds.conf.default
+#sed -i '2d' feeds.conf.default
+#sed -i '2i src-git luci https://github.com/shineTripodcat/luci.git;openwrt-21.02' feeds.conf.default
 
 
 # 修改版本为编译日期
