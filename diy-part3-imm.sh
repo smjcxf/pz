56ac957fd43e3fd2ebfc03737d0dfa9b56caf2cb
shineTripodcat <111100023+shineTripodcat@users.noreply.github.com>
2024-09-25 20:06:22 +0800

    Update diy-part3-imm.sh

--- diy-part3-imm.sh
+++ diy-part3-imm.sh
@@ -7,11 +7,11 @@
 
 
 # 删除现有的 luci-app-passwall 软件包
-rm -rf feeds/luci/applications/luci-app-passwall
+# rm -rf feeds/luci/applications/luci-app-passwall
 
 # 克隆新的 luci-app-passwall 软件包
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5 feeds/packages/mosdns
+# git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5 feeds/packages/mosdns
 
 # 移动克隆的文件到正确的目录
 #mv package/luci-app-passwall/luci-app-passwall feeds/luci/applications/
@@ -19,6 +19,6 @@ git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5 feeds/package
 # 删除临时克隆目录
 #rm -rf package/luci-app-passwall
 # 删除 /feeds/passwall_package/sing-box 目录
-rm -rf feeds/passwall_package/sing-box
+# rm -rf feeds/passwall_package/sing-box
 
 ./scripts/feeds update -a
